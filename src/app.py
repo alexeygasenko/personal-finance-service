858f81e7a110443cc43072327af23b4eab023d35
JonnyPovidlos <jonnypovidlos@yandex.ru>
2020-06-13 22:14:39 +0500

    fix(create_db.py)

--- src/app.py
+++ src/app.py
@@ -7,5 +7,5 @@ def create_app():
 	app = Flask(__name__)
 	app.config.from_object('config.Config')
 	if not os.path.exists(os.path.join(os.getcwd(), app.config['DB_CONNECTION'])):
-		create_db(app, app.config['DB_CONNECTION'])
+		create_db(app)
 	return app
