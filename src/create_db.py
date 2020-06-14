bfbd9fd059a539510459709bf4bf94d32bb70f1c
JonnyPovidlos <jonnypovidlos@yandex.ru>
2020-06-15 02:53:33 +0500

    fix(create_db): изменение модели баззы данных
    -в таблицу category добавлено поле user_id

--- src/create_db.py
+++ src/create_db.py
@@ -8,7 +8,9 @@ def create_db(app):
 			id              INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
 			title           TEXT NOT NULL, 
 			parent_category INTEGER, 
-			FOREIGN KEY(parent_category) REFERENCES category(id) 
+			user_id         INTEGER NOT NULL,
+			FOREIGN KEY(parent_category) REFERENCES category(id),
+			FOREIGN KEY(user_id) REFERENCES user(id) 
 			); 
 			
 			CREATE TABLE user (
